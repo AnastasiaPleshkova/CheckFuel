cf5623472f3931686e1d783bb8824edda3725039
Anastasia <anastasiiapleshkova@gmail.com>
2023-09-20 09:39:46 +0300

    added new app icon

--- src/main/java/ru/pleshkova/checkfuel/HelloApplication.java
+++ src/main/java/ru/pleshkova/checkfuel/HelloApplication.java
@@ -3,6 +3,7 @@ package ru.pleshkova.checkfuel;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+        stage.getIcons().add(new Image(HelloApplication.class.getResourceAsStream("images/icon.png")));
         stage.setTitle("Check Fuel FX");
         stage.setScene(scene);
         stage.show();
